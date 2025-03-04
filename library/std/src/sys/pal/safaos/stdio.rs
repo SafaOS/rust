53ba9baba4cc42a11ba580f5ccbcd4020d354af2
Roxve <116169309+Roxve@users.noreply.github.com>
2025-03-04 19:59:13 +0200

    [library stdio]: Stdin Read implementition and a few more Error related changes

--- library/std/src/sys/pal/safaos/stdio.rs
+++ library/std/src/sys/pal/safaos/stdio.rs
@@ -1,22 +1,35 @@
 use super::syscalls;
 use crate::io;
+
+#[stable(feature = "stdio", since = "1.0.0")]
+impl From<syscalls::ErrorStatus> for io::Error {
+    fn from(err: syscalls::ErrorStatus) -> io::Error {
+        let kind = err.into_io_error_kind();
+        let error = err.as_str();
+
+        io::Error::new(kind, error)
+    }
+}
+
 pub struct Stdin;
 pub struct Stdout;
 pub struct Stderr;
 
 impl Stdin {
+    pub const FD: usize = 0;
     pub const fn new() -> Stdin {
         Stdin
     }
 }
 
 impl io::Read for Stdin {
-    fn read(&mut self, _buf: &mut [u8]) -> io::Result<usize> {
-        Ok(0)
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        Ok(syscalls::read(Self::FD, -1, buf)?)
     }
 }
 
 impl Stdout {
+    const FD: usize = 1;
     pub const fn new() -> Stdout {
         Stdout
     }
@@ -24,37 +37,36 @@ impl Stdout {
 
 impl io::Write for Stdout {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let mut dest_wrote = 0;
-        syscalls::syswrite(1, -1, buf.as_ptr(), buf.len(), &mut dest_wrote);
-        Ok(dest_wrote)
+        Ok(syscalls::write(Self::FD, -1, buf)?)
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        syscalls::syssync(1);
-        Ok(())
+        Ok(syscalls::sync(1)?)
     }
 }
 
 impl Stderr {
+    // TODO: please add a seprate stderr fd
+    const FD: usize = Stdout::FD;
     pub const fn new() -> Stderr {
         Stderr
     }
 }
 
 impl io::Write for Stderr {
+    #[inline]
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let mut dest_wrote = 0;
-        syscalls::syswrite(2, -1, buf.as_ptr(), buf.len(), &mut dest_wrote);
-        Ok(dest_wrote)
+        let wrote = syscalls::write(Self::FD, -1, buf)?;
+        self.flush()?;
+        Ok(wrote)
     }
-
+    #[inline]
     fn flush(&mut self) -> io::Result<()> {
-        syscalls::syssync(2);
-        Ok(())
+        Ok(syscalls::sync(Self::FD)?)
     }
 }
 
-pub const STDIN_BUF_SIZE: usize = 0;
+pub const STDIN_BUF_SIZE: usize = 1024;
 
 pub fn is_ebadf(_err: &io::Error) -> bool {
     true
