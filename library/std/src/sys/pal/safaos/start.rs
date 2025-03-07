bd8f60a9a1db35e059071d9e3198330fe357495f
Roxve <116169309+Roxve@users.noreply.github.com>
2025-03-07 19:55:00 +0200

    [library/std pal args]: implemented args for safaos

--- library/std/src/sys/pal/safaos/start.rs
+++ library/std/src/sys/pal/safaos/start.rs
@@ -1,13 +1,35 @@
-use crate::ffi::{c_char, c_int};
-use crate::ptr;
+use core::ptr::NonNull;
+
+use super::args::RAW_ARGS;
+use super::args::RawArgs;
+use super::os::exit;
+use crate::mem::MaybeUninit;
 
 extern "C" {
-    fn main(argc: c_int, argv: *const *const c_char) -> c_int;
+    fn main() -> u16;
+}
+
+unsafe fn _start_inner(argc: usize, argv: *mut (NonNull<u8>, usize)) -> ! {
+    unsafe {
+        let raw_args = match argc == 0 || argv.is_null() {
+            true => None,
+            false => {
+                let args_slice = core::slice::from_raw_parts_mut(argv, argc);
+                let args = NonNull::new_unchecked(args_slice as *mut _);
+                Some(RawArgs { args })
+            }
+        };
+
+        RAW_ARGS.get().write(MaybeUninit::new(raw_args));
+        let results = main();
+
+        exit(results as i32)
+    }
 }
 
 #[no_mangle]
 #[allow(unused)]
-pub extern "C" fn _start() {
+pub extern "C" fn _start(argc: usize, argv: *mut (NonNull<u8>, usize)) {
     unsafe {
         core::arch::asm!(
             "
@@ -17,14 +39,6 @@ pub extern "C" fn _start() {
         ",
             options(nostack)
         );
-        main(0, ptr::null());
-        core::arch::asm!(
-            "
-            xor rax, rax
-            xor rdi, rdi
-            int 0x80
-        ",
-            options(noreturn)
-        );
+        _start_inner(argc, argv);
     };
 }
