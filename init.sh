5ae510ef078eea6862ad64833918b0bf55650d5d
ObserverUnit <116169309+ObserverUnit@users.noreply.github.com>
2025-03-19 19:16:14 +0200

    [library] fix core missing

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 # initalizes submodules required to compile std for SafaOS
 git submodule init library/stdarch  src/llvm-project library/backtrace
-git submodule update
+git submodule update --depth 1
