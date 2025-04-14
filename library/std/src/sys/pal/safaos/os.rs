95b8ec65c59bbe39e1772862239c35fb7d917efc
ObserverUnit <116169309+ObserverUnit@users.noreply.github.com>
2025-04-14 12:45:39 +0200

    [library/std pal::safaos]: update safa-api to 0.2.6, change how arguments are handled to use api implementition and add environment variables

--- library/std/src/sys/pal/safaos/os.rs
+++ library/std/src/sys/pal/safaos/os.rs
@@ -80,45 +80,61 @@ pub fn current_exe() -> io::Result<PathBuf> {
     unsupported()
 }
 
-pub struct Env(!);
+pub struct Env {
+    inner: Vec<(OsString, OsString)>,
+    index: usize,
+}
 
 impl Env {
     // FIXME(https://github.com/rust-lang/rust/issues/114583): Remove this when <OsStr as Debug>::fmt matches <str as Debug>::fmt.
     pub fn str_debug(&self) -> impl fmt::Debug + '_ {
-        let Self(inner) = self;
-        match *inner {}
+        self
     }
 }
 
 impl fmt::Debug for Env {
-    fn fmt(&self, _: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let Self(inner) = self;
-        match *inner {}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_list().entries(self.inner.iter()).finish()
     }
 }
 
 impl Iterator for Env {
     type Item = (OsString, OsString);
     fn next(&mut self) -> Option<(OsString, OsString)> {
-        let Self(inner) = self;
-        match *inner {}
+        if self.index >= self.inner.len() {
+            return None;
+        }
+        let (key, value) = self.inner[self.index].clone();
+        self.index += 1;
+        Some((key, value))
     }
 }
 
 pub fn env() -> Env {
-    panic!("not supported on this platform")
+    let all = safa_api::process::env_get_all();
+    let mut results = Vec::with_capacity(all.len());
+    for (key, value) in all {
+        results.push((unsafe { OsString::from_encoded_bytes_unchecked(key.to_vec()) }, unsafe {
+            OsString::from_encoded_bytes_unchecked(value.to_bytes().to_vec())
+        }));
+    }
+
+    Env { inner: results, index: 0 }
 }
 
-pub fn getenv(_: &OsStr) -> Option<OsString> {
-    None
+pub fn getenv(key: &OsStr) -> Option<OsString> {
+    safa_api::process::env_get(key.as_encoded_bytes())
+        .map(|s| unsafe { OsString::from_encoded_bytes_unchecked(s.to_vec()) })
 }
 
-pub unsafe fn setenv(_: &OsStr, _: &OsStr) -> io::Result<()> {
-    Err(io::const_error!(io::ErrorKind::Unsupported, "cannot set env vars on this platform"))
+pub unsafe fn setenv(key: &OsStr, value: &OsStr) -> io::Result<()> {
+    safa_api::process::env_set(key.as_encoded_bytes(), value.as_encoded_bytes());
+    Ok(())
 }
 
-pub unsafe fn unsetenv(_: &OsStr) -> io::Result<()> {
-    Err(io::const_error!(io::ErrorKind::Unsupported, "cannot unset env vars on this platform"))
+pub unsafe fn unsetenv(key: &OsStr) -> io::Result<()> {
+    safa_api::process::env_remove(key.as_encoded_bytes());
+    Ok(())
 }
 
 pub fn temp_dir() -> PathBuf {
