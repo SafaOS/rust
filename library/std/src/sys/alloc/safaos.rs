53891fdc2f20fdead01e53fe351cc00fa515a4ca
safiworks <116169309+safiworks@users.noreply.github.com>
2025-07-29 21:23:29 +0300

    chore(library/std) move alloc.rs from sys/pal/safaos to sys/alloc/safaos.rs

---
+++ library/std/src/sys/alloc/safaos.rs
@@ -0,0 +1,24 @@
+use safa_api::alloc::GLOBAL_SYSTEM_ALLOCATOR;
+
+use crate::alloc::{GlobalAlloc, Layout, System};
+
+#[stable(feature = "alloc_system_type", since = "1.28.0")]
+unsafe impl GlobalAlloc for System {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let results = unsafe { GLOBAL_SYSTEM_ALLOCATOR.alloc(layout) };
+        results
+    }
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        unsafe {
+            GLOBAL_SYSTEM_ALLOCATOR.dealloc(ptr, layout);
+        }
+    }
+
+    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
+        unsafe { GLOBAL_SYSTEM_ALLOCATOR.alloc_zeroed(layout) }
+    }
+
+    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
+        unsafe { GLOBAL_SYSTEM_ALLOCATOR.realloc(ptr, layout, new_size) }
+    }
+}
--- library/std/src/sys/pal/safaos/mod.rs
+++ library/std/src/sys/pal/safaos/mod.rs
@@ -1,6 +1,5 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 
-pub mod alloc;
 pub mod os;
 pub mod pipe;
 pub mod resources;
